35d83246f182114a9b83558af1b253d611f62ced
Wkyouma <igorterplak297@gmail.com>
2025-01-10 19:32:14 -0300

    sectionss

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,7 @@ const Home = () => {
             <Section
                 title="Validation"
                 subtitle="teste | teste"
-                bgImage="/Galeria/teste1.jpg"
+                bgImage="/Galeria/teste.jpg"
                 borderColor="border-green-500"
                 buttonColor="bg-green-600"
                 textColor="text-green-300"
@@ -15,7 +15,7 @@ const Home = () => {
             <Section
                 title="Bittersweet Memories"
                 subtitle="teste | teste"
-                bgImage="/Galeria/teste2.jpg"
+                bgImage="/Galeria/teste1.jpg"
                 borderColor="border-blue-500"
                 buttonColor="bg-blue-600"
                 textColor="text-blue-300"
@@ -24,7 +24,7 @@ const Home = () => {
             <Section
                 title="13 Lentes de um final feliz"
                 subtitle="teste | teste"
-                bgImage="/Galeria/teste.jpg"
+                bgImage="/Galeria/teste2.jpg"
                 borderColor="border-pink-500"
                 buttonColor="bg-pink-600"
                 textColor="text-pink-300"
