12b67cbc6ed1c4f7da50998f07b2b9f530ddec60
Wkyouma <igorterplak297@gmail.com>
2025-01-25 15:21:00 -0300

    testes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './Routes.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './Routes.jsx';
+import { ResponsiveProvider } from './context/ResponsiveContext'// Importe o ResponsiveProvider
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ResponsiveProvider> {/* Envolva o App com o ResponsiveProvider */}
+      <App />
+    </ResponsiveProvider>
   </StrictMode>,
-)
+);
