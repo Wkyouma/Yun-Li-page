d6c81b7442a66fb174b0657947cdfe4fc240c2bd
Wkyouma <igorterplak297@gmail.com>
2025-03-04 22:05:28 -0300

    teste

--- src/pages/Loja/index.jsx
+++ src/pages/Loja/index.jsx
@@ -5,14 +5,11 @@ import product  from './Loja.json'
 const Loja = () => {
   return (
     <div className="bg-gradient-to-t from-slate-700 to-black h-auto">
-      {/* Seção do Carrossel */}
       <div className="bg-transparent py-16">
         <div className="mx-auto px-4">
           <Carousel />
         </div>
       </div>
-
-      {/* Seção do Conteúdo */}
       <div className="flex flex-col items-center ">
         <h1 className="text-4xl md:text-5xl text-center text-white mb-5">Produtos</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
